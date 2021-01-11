3fdcb756df39db1fd7bd7127489e2d3c95d5feca
joacim wall <joacim.wall@evry.com>
2021-01-11 10:21:01 +0100

    Add demo code

---
+++ Add multilingual support to Xamarin/MLDemo/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MLDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //sample of hard set language that not need to match language on device
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+            MLDemo.Resources.AppResources.Culture = Thread.CurrentThread.CurrentUICulture;
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Add multilingual support to Xamarin/MLDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MLDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+            var translation = MLDemo.Resources.AppResources.C_Sharp_Is_Love; 
+
+        }
+    }
+}
