17a89181a8672dda5643c2a1438519a3cac50e8a
joacim wall <joacim.wall@evry.com>
2021-01-11 19:09:55 +0100

    Add Grpc Demo

---
+++ gRPC_Xamarin/Grpc.Client/GrpcDemo/Services/GreeterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GrpcDemo.Services
+{
+    public class GreeterService
+    {
+        private Grpc.Server.Greeter.GreeterClient _greeterClient;
+
+        public GreeterService()
+        {
+            _greeterClient = new Grpc.Server.Greeter.GreeterClient();
+        }
+    }
+}
